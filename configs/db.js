682ac7a771422f141d376c5d4fc75dd0b9af2826
infosanuth <infosanuth@gmail.com>
2026-04-28 19:21:22 +0530

    feat: setup mongodb connection

---
+++ configs/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${uri}/nodejs-ec2`);
+        console.log("MongoDB connection established");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
