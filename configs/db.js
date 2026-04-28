b2d2ae102e741834a59f1650cea28eb5ef8b69d0
infosanuth <infosanuth@gmail.com>
2026-04-28 21:50:50 +0530

    fix: resolve mongodb connection and es module import issues

--- configs/db.js
+++ configs/db.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI;
-
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${uri}/nodejs-ec2`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/nodejs-ec2`);
         console.log("MongoDB connection established");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
